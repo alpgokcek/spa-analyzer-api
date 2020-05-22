10d8d2d9dd68e18de55616772fe44ffcff794739
alpgokcek <alp.gokcek1@gmail.com>
2020-05-22 09:53:38 +0300

    update

--- app/Http/Controllers/Api/CheckAuth.php
+++ app/Http/Controllers/Api/CheckAuth.php
@@ -16,10 +16,10 @@ class CheckAuth extends ApiController
     public function index(Request $request)
     {
         $user = User::find(Auth::id()); // oturum açan kişinin bilgilerini buradan alıyoruz.
-        if ($user->level == 6) {
-            return $this->apiResponse(ResaultType::Error, 403, 'Authorization Error', 0, 403);
+        if ($user->level == 1 || $user->level == 5) {
+						return $this->apiResponse(ResaultType::Success, 204, NULL, 0, 204);
         } else{
-            return $this->apiResponse(ResaultType::Success, 204, NULL, 0, 204);
+						return $this->apiResponse(ResaultType::Error, 403, 'Authorization Error', 0, 403);
         }
     }
 }
