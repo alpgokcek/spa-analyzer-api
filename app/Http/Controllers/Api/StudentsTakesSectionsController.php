87a74e1c5b107de2bdb44f2721a23f41e3210e42
alpgokcek <alp.gokcek1@gmail.com>
2021-03-18 11:59:09 +0300

    sts controller update

--- app/Http/Controllers/Api/StudentsTakesSectionsController.php
+++ app/Http/Controllers/Api/StudentsTakesSectionsController.php
@@ -133,12 +133,16 @@ class StudentsTakesSectionsController extends ApiController
 
     }
 
-
-
-
     public function show($id)
     {
-        $data = StudentsTakesSections::find($id);
+        #$data = StudentsTakesSections::find($id);
+				$query = StudentsTakesSections::query();
+				$query->join('users','users.student_id','=','students_takes_sections.student_id');
+        $query->join('section','section.id','=','students_takes_sections.section_id');
+				$query->join('course','course.id','=','section.course_id');
+        $query->where('students_takes_sections.id', '=', $id);
+        $query->select('course.code as course_code', 'course.id as course_id', 'course.title as course_name', 'users.name as user_name', 'section.title as section_title', 'students_takes_sections.*');
+				$data = $query->get();
         if ($data) {
             return $this->apiResponse(ResaultType::Success, $data, 'StudentsTakesSections Detail', 201);
         } else {
