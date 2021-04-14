b6a40f472a64eb63fd6de87b3cf5331b89539e2d
alpgokcek <alp.gokcek1@gmail.com>
2021-04-14 15:27:33 +0300

    gtcco update.

--- app/Http/Controllers/Api/GradingToolCoversCourseOutcomeController.php
+++ app/Http/Controllers/Api/GradingToolCoversCourseOutcomeController.php
@@ -26,19 +26,21 @@ class GradingToolCoversCourseOutcomeController extends ApiController
 								$query->join('grading_tool','grading_tool.id','=','grading_tool_covers_course_outcome.grading_tool_id');
 								$query->join('course','course.id','=','course_outcome.course_id');
                 $query->join('department','department.id','=','course.department_id');
+								$query->join('assessment','assessment.id','=','grading_tool.assessment_id');
 
                 $query->where('department.faculty','=',$user->faculty_id);
 
-                $query->select('course_outcome.code as co_code', 'grading_tool.question_number as question_number', 'grading_tool_covers_course_outcome.*');
+                $query->select('assessment.name as assessment_name','course_outcome.code as co_code', 'grading_tool.question_number as question_number', 'grading_tool_covers_course_outcome.*');
             break;
 
             case 4:
                 $query->join('course_outcome','course_outcome.id','=','grading_tool_covers_course_outcome.course_outcome_id');
 								$query->join('grading_tool','grading_tool.id','=','grading_tool_covers_course_outcome.grading_tool_id');
                 $query->join('course','course.id','=','course_outcome.course_id');
+								$query->join('assessment','assessment.id','=','grading_tool.assessment_id');
 
                 $query->where('course.department_id','=',$user->department_id);
-                $query->select('course_outcome.code as co_code', 'grading_tool.question_number as question_number', 'grading_tool_covers_course_outcome.*');
+                $query->select('assessment.name as assessment_name','course_outcome.code as co_code', 'grading_tool.question_number as question_number', 'grading_tool_covers_course_outcome.*');
             break;
 
             case 5:
@@ -47,10 +49,11 @@ class GradingToolCoversCourseOutcomeController extends ApiController
                 $query->join('course','course.id','=','course_outcome.course_id');
                 $query->join('section','section.course_id','=','course.id');
                 $query->join('instructors_gives_sections','instructors_gives_sections.section_id','=','section.id');
+								$query->join('assessment','assessment.id','=','grading_tool.assessment_id');
 
                 $query->where('instructors_gives_sections.instructor_id','=',$user->id);
 
-                $query->select('course_outcome.code as co_code', 'grading_tool.question_number as question_number', 'grading_tool_covers_course_outcome.*');
+                $query->select('assessment.name as assessment_name','course_outcome.code as co_code', 'grading_tool.question_number as question_number', 'grading_tool_covers_course_outcome.*');
             break;
             case 6:
                 // 6. seviyenin bu ekranda işi olmadığı için 403 verip gönderiyoruz.
@@ -62,7 +65,8 @@ class GradingToolCoversCourseOutcomeController extends ApiController
                 //$query->where('student_gets_measured_grade_program_outcome.student_id','=',$user->id);
 								$query->join('course_outcome','course_outcome.id','=','grading_tool_covers_course_outcome.course_outcome_id');
 								$query->join('grading_tool','grading_tool.id','=','grading_tool_covers_course_outcome.grading_tool_id');
-                $query->select('course_outcome.code as co_code', 'grading_tool.question_number as question_number', 'grading_tool_covers_course_outcome.*');
+								$query->join('assessment','assessment.id','=','grading_tool.assessment_id');
+                $query->select('assessment.name as assessment_name','course_outcome.code as co_code', 'grading_tool.question_number as question_number', 'grading_tool_covers_course_outcome.*');
             break;
         }
 
@@ -132,8 +136,9 @@ class GradingToolCoversCourseOutcomeController extends ApiController
 				$query = GradingToolCoversCourseOutcome::query();
 				$query->join('course_outcome','course_outcome.id','=','grading_tool_covers_course_outcome.course_outcome_id');
 				$query->join('grading_tool','grading_tool.id','=','grading_tool_covers_course_outcome.grading_tool_id');
+				$query->join('assessment','assessment.id','=','grading_tool.assessment_id');
 				$query->where('grading_tool_covers_course_outcome.id', '=', $id);
-				$query->select('course_outcome.code as co_code', 'grading_tool.question_number as question_number', 'grading_tool_covers_course_outcome.*');
+				$query->select('assessment.name as assessment_name','course_outcome.code as co_code', 'grading_tool.question_number as question_number', 'grading_tool_covers_course_outcome.*');
 				$data = $query->get()->first();
         if ($data) {
             return $this->apiResponse(ResultType::Success, $data, 'GradingToolCoversCourseOutcome Detail', 201);
